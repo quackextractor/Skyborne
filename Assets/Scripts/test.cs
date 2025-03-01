dacca00dea9ee1d73b4af53a1e1c1d17f44464db
LostSoul <miro.slezak0601@gmail.com>
2025-03-01 18:26:30 +0100

    Test

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame update WAUH(DSHU(DHU(A
     void Start()
     {
         
